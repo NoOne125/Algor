e4be36e71a7670683fd952523f42b5299f8aa884
NoOne125 <74257251+NoOne125@users.noreply.github.com>
2021-12-28 14:35:45 +0200

    Add files via upload

---
+++ Лабораторная 5/Лабораторная 5/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Лабораторная_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Plum;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 25);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(86, 24);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = "Лёгкая";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 55);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(132, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Нормальная";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 85);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(97, 24);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.Text = "Трудная";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 418);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(211, 143);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Сложность";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 22.25F);
+            this.label1.Location = new System.Drawing.Point(229, 424);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(188, 131);
+            this.label1.TabIndex = 3;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.ClientSize = new System.Drawing.Size(429, 573);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Реверсы";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Лабораторная 5/Лабораторная 5/Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лабораторная_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool start = false;
+        int moves = 1;
+        int complexity = 0;
+        int[,] field = new int[8, 8];
+        Graphics gr;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gr = pictureBox1.CreateGraphics();
+
+            label1.Text = "";
+
+            start = true;
+            moves = 0;
+            field = new int[8, 8];
+
+            if (radioButton1.Checked)
+                complexity = 0;
+            else if (radioButton2.Checked)
+                complexity = 1;
+            else
+                complexity = 2;
+
+            Draw_Field();
+            moves++;
+            Draw_Figure(3, 4);
+            moves++;
+            Draw_Figure(3, 3);
+            moves++;
+            Draw_Figure(4, 3);
+            moves++;
+            Draw_Figure(4, 4);
+
+            moves = 0;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (start)
+            {
+                int cur_x = (PointToClient(Cursor.Position).X - 15) / 50;
+                int cur_y = (PointToClient(Cursor.Position).Y - 15) / 50;
+
+                if (field[cur_x, cur_y] == 0)
+                {
+                    moves++;
+
+                    if (Result_Move(cur_x, cur_y, false, field))
+                    {
+                        Draw_Figure(cur_x, cur_y);
+                        if (IsFinal(field))
+                            Final();
+                        else
+                        {
+                            Bot_Turn();
+                        }
+                    }
+                    else
+                        moves--;
+                }
+            }
+        }
+
+        void Draw_Field()
+        {
+            gr.Clear(Color.Plum);
+            Pen black = new Pen(Brushes.Black);
+            for (int i = 1; i < 8; i++)
+            {
+                gr.DrawLine(black, i * 50, 0, i * 50, 400);
+                gr.DrawLine(black, 0, i * 50, 400, i * 50);
+            }
+        }
+
+        void Draw_Figure(int x, int y)
+        {
+            Brush circle;
+            if (moves % 2 == 1)
+                circle = Brushes.Black;
+            else
+                circle = Brushes.White;
+
+            field[x, y] = moves % 2 + 1;
+
+            x = x * 50 + 5;
+            y = y * 50 + 5;
+            gr.FillEllipse(circle, x, y, 40, 40);
+        }
+
+        void Bot_Turn()
+        {
+            moves++;
+
+            int[,] cur = new int[8, 8];
+
+            for (int r = 0; r < 8; r++)
+            {
+                for (int t = 0; t < 8; t++)
+                {
+                    cur[r, t] = field[r, t];
+                }
+            }
+
+            Minimax(field, complexity * 2 + 1, -65, 65, true, ref cur);
+
+            field = cur;
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (field[i, j] == 1)
+                    {
+                        Draw_Figure(i, j);
+                    }
+                }
+            }
+            if (IsFinal(field))
+                Final();
+        }
+
+        bool IsFinal(int[,] loc_field)
+        {
+            bool end = true;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (loc_field[i, j] == (moves + 1) % 2 + 1)
+                    {
+                        moves++;
+                        end = !Result_Move(i, j, true, loc_field);
+                        moves--;
+                        if (!end)
+                            return end;
+                    }
+                }
+            }
+            return end;
+        }
+
+        bool Result_Move(int x, int y, bool test, int[,] loc_field)
+        {
+            int cur = moves % 2 + 1;
+            bool good_res = false;
+            for (int i = -1; i <= 1; i++)
+            {
+                for(int j = -1; j <= 1; j++)
+                {
+                    if (i == j && i==0)
+                        continue;
+                    int loc_i = 0;
+                    int x_loc = x;
+                    int y_loc = y;
+                    do
+                    {
+                        loc_i++;
+                        x_loc += i;
+                        y_loc += j;
+                        if (x_loc == 8 || y_loc == 8 || x_loc == -1 || y_loc == -1)
+                            break;
+                        if(loc_field[x_loc, y_loc] == 0)
+                        {
+                            if (test && loc_i>1)
+                            {
+                                return true;
+                            }
+                            break;
+                        }
+                        if (loc_field[x_loc, y_loc] != cur) 
+                            continue;
+                        if (loc_field[x_loc, y_loc] == cur)
+                        {
+                            if (!test)
+                            {
+                                if (loc_i - 1 != 0)
+                                    good_res = true;
+                            }
+
+                            for (int k = 0; k < loc_i - 1; k++)
+                            {
+                                x_loc -= i;
+                                y_loc -= j;
+
+                                if (!test)
+                                {
+                                    Draw_Figure(x_loc, y_loc);
+                                    field[x_loc, y_loc] = cur;
+                                }
+                            }
+
+                            break;
+                        }
+
+                    } while (true);
+                }
+            }
+
+            return good_res;
+        }
+
+        void Final()
+        {
+            start = false;
+            int N_black = 0;
+            int N_white = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (field[i, j] == 2)
+                        N_black++;
+                    else if (field[i, j] == 1)
+                        N_white++;
+                }
+            }
+            if (N_white > N_black) 
+                label1.Text = "Победа Белых";
+            else if(N_black > N_white)
+                label1.Text = "Победа Чёрных";
+            else
+                label1.Text = "Победила Дружба";
+        }
+
+        int Minimax(int[,] loc_field, int depth, int alpha, int beta, bool maxPlay, ref int[,] res_field)
+        {
+            int[,] cur = new int[8, 8];
+
+            for (int r = 0; r < 8; r++)
+            {
+                for (int t = 0; t < 8; t++)
+                {
+                    cur[r, t] = loc_field[r, t];
+                }
+            }
+
+            if (depth == 0 || IsFinal(cur))
+            {
+                return Cool_Game(cur);
+            }
+
+            if (maxPlay)
+            {
+                int max = -65;
+                List<int[,]> loc = HightWay_ToHell(1, cur);
+                foreach (int[,] k in loc)
+                {
+                    int val = Minimax(k, depth - 1, alpha, beta, false, ref res_field);
+                    if (max < val)
+                    {
+                        max = val;
+                        if (depth == complexity * 2 + 1)
+                        {
+                            res_field = k;
+                        }
+                    }
+                    alpha = Math.Max(alpha, val);
+                    if (beta <= alpha)
+                        break;
+                }
+                return max;
+            }
+            else
+            {
+                int min = 65;
+                List<int[,]> loc = HightWay_ToHell(2, cur);
+                foreach (int[,] k in loc)
+                {
+                    int val = Minimax(k, depth - 1, alpha, beta, true, ref res_field);
+                    min = Math.Min(min, val);
+                    beta = Math.Min(beta, val);
+                    if (beta <= alpha)
+                        break;
+                }
+                return min;
+            }
+        }
+
+        List<int[,]> HightWay_ToHell(int color, int[,] loc_field)
+        {
+            List<int[,]> turns = new List<int[,]>();
+            for (int k = 0; k < 8; k++)
+            {
+                for (int l = 0; l < 8; l++)
+                {
+                    if (loc_field[k, l] == 0)
+                    {
+                        bool edit = false;
+                        int[,] new_field = new int[8, 8];
+                        for (int r = 0; r < 8; r++)
+                        {
+                            for (int t = 0; t < 8; t++)
+                            {
+                                new_field[r, t] = loc_field[r, t];
+                            }
+                        }
+                        for (int i = -1; i <= 1; i++)
+                        {
+                            for (int j = -1; j <= 1; j++)
+                            {
+                                if (i == j && i == 0)
+                                    continue;
+                                int loc_i = 0;
+                                int x_loc = k;
+                                int y_loc = l;
+                                do
+                                {
+                                    loc_i++;
+                                    x_loc += i;
+                                    y_loc += j;
+                                    if (x_loc == 8 || y_loc == 8 || x_loc == -1 || y_loc == -1)
+                                        break;
+                                    if (new_field[x_loc, y_loc] == 0)
+                                        break;
+                                    if (new_field[x_loc, y_loc] != color)
+                                        continue;
+                                    if (new_field[x_loc, y_loc] == color)
+                                    {
+                                        if (loc_i > 1)
+                                            edit = true;
+                                        for (int m = 0; m < loc_i; m++)
+                                        {
+                                            x_loc -= i;
+                                            y_loc -= j;
+
+                                            new_field[x_loc, y_loc] = color;
+                                        }
+                                        break;
+                                    }
+                                } while (true);
+                            }
+                        }
+                        if (edit)
+                            turns.Add(new_field);
+                    }
+                }
+            }
+            return turns;
+        }
+
+        int Cool_Game(int[,] fieled)
+        {
+            int scores = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (fieled[i, j] == 1)
+                    {
+                        scores++;
+                    }
+                    else if (fieled[i, j] == 2)
+                        scores--;
+                }
+            }
+            return scores;
+        }
+
+    }
+}
